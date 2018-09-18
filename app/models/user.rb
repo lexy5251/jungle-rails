5c220c1174afe44b8929574be6840644a3944233
lexy5251 <xixi5251@gmail.com>
2018-09-18 01:53:15 +0000

    before space sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
+
+  validates :email, uniqueness: {case_sensitive: false}
+
+  validates :password, confirmation: true, length: {minimum: 4}
+
+  def self.authenticate_with_credentials (email, password)
+    @user = User.find_by(email: email)
+
+    if @user && @user.try(:authenticate, password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
---
+++ spec/user_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    before do
+      @user = User.create(first_name: "Xi", last_name: "Yu", email: "XYU20@art.edu", password: "abc", password_confirmation: "abc")
+
+    end
+
+    # it "should be valid when user is saved" do
+    #   expect(@user.save).to eq(true)
+    # end
+
+    it "should be invalid because the password does not match" do
+      @user.password_confirmation = "123"
+      expect(@user).to_not be_valid
+    end
+
+    it "should be invalid because the email is not case sensitive" do
+      user = User.create(first_name: "Xi", last_name: "Yu", email: "xyu20@ART.edu", password: "abc", password_confirmation: "abc")
+      expect(user).to_not be_valid
+    end
+
+    it 'should not be valid since email is not valid' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+
+    end
+
+    it 'should not be valid since first_name is not valid' do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+
+    end
+
+    it 'should not be valid since last_name is not valid' do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+
+    end
+
+    it 'should not be valid since the length of password is one' do
+      @user.password = "a"
+      expect(@user).to_not be_valid
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    before do
+      @user = User.create(first_name: "Xi", last_name: "Yu", email: "XYU20@art.edu", password: "abcd", password_confirmation: "abcd")
+    end
+
+    it 'should give me an user' do
+      @test_user = User.authenticate_with_credentials("XYU20@art.edu", "abcd")
+
+      expect(@test_user).to eq(@user)
+    end
+
+    it 'should not be valid since the email has a space' do
+    @test_user = User.authenticate_with_credentials(" XYU20@art.edu", "abcd")
+
+      expect(@test_user).to eq(@user)
+    end
+  end
+
+
+
+
+
+
+
+end
